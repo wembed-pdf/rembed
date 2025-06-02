efd82b630ccad72e333e4c12addc9b18104a48a0
Tobias Kempf <kempf.tobias@web.de>
2025-06-02 21:08:34 +0200

    Add position generator

--- benchmark/src/lib.rs
+++ benchmark/src/lib.rs
@@ -1,3 +1,5 @@
 mod generate_graphs;
+mod generate_positions;
 
 pub use generate_graphs::GraphGenerator;
+pub use generate_positions::GraphGenerator as PositionGenerator;
--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,4 +1,5 @@
 use benchmark::GraphGenerator;
+use benchmark::PositionGenerator;
 
 fn main() {
     let graph_generator = GraphGenerator {
@@ -6,4 +7,12 @@ fn main() {
         output_path: "../data/generated".to_string(),
     };
     graph_generator.generate();
+
+    let position_generator = PositionGenerator {
+        wembed_path: "./../../wembed/release/bin/cli_wembed".to_string(),
+        graphs_path: "../data/generated/wembed_graphs".to_string(),
+        output_path: "../data/generated/positions".to_string(),
+    };
+    position_generator.change_graphs("../data/generated/graphs".to_string());
+    position_generator.generate();
 }
