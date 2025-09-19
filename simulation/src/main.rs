78429133d167dd3b8feaf3ab0821cc635ef876d2
Dennis Kobert <dennis@kobert.dev>
2025-09-19 14:16:30 +0200

    Add simulation package for real

---
+++ simulation/src/main.rs
@@ -0,0 +1,76 @@
+use rembed::{dim_reduction::LayeredLsh, embedder::EmbedderOptions, *};
+use simulation::radius_reduction::Statistics;
+
+fn main() -> io::Result<()> {
+    let dim = 8;
+    let dim_hint = 8;
+
+    let graph = "../data/generated/graphs/19_girg_n-1000_deg-15_dim-4_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
+    // let graph = "../data/generated/graphs/55_girg_n-10000_deg-15_dim-2_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
+    // let graph = "../data/generated/graphs/109_girg_n-100000_deg-15_dim-2_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
+    // let graph = "../data/generated/graphs/1217_girg_n-1000000_deg-15_dim-4_ple-2.5_alpha-inf_wseed-12_pseed-130_sseed-1400";
+    let graph = graph::Graph::parse_from_edge_list_file(graph, dim, dim_hint)?;
+
+    embedd_and_calc_stats::<2>(&graph);
+    embedd_and_calc_stats::<3>(&graph);
+    embedd_and_calc_stats::<4>(&graph);
+    embedd_and_calc_stats::<5>(&graph);
+    embedd_and_calc_stats::<6>(&graph);
+    embedd_and_calc_stats::<7>(&graph);
+    embedd_and_calc_stats::<8>(&graph);
+    embedd_and_calc_stats::<9>(&graph);
+    embedd_and_calc_stats::<10>(&graph);
+    embedd_and_calc_stats::<11>(&graph);
+    embedd_and_calc_stats::<12>(&graph);
+    embedd_and_calc_stats::<13>(&graph);
+    embedd_and_calc_stats::<14>(&graph);
+    embedd_and_calc_stats::<15>(&graph);
+    embedd_and_calc_stats::<16>(&graph);
+    embedd_and_calc_stats::<17>(&graph);
+    embedd_and_calc_stats::<18>(&graph);
+    embedd_and_calc_stats::<19>(&graph);
+    embedd_and_calc_stats::<20>(&graph);
+    embedd_and_calc_stats::<21>(&graph);
+    embedd_and_calc_stats::<22>(&graph);
+    embedd_and_calc_stats::<23>(&graph);
+    embedd_and_calc_stats::<24>(&graph);
+
+    Ok(())
+}
+
+fn embedd_and_calc_stats<const D: usize>(graph: &graph::Graph) {
+    let options = EmbedderOptions {
+        max_iterations: 500,
+        ..Default::default()
+    };
+    let mut embedder: embedder::WEmbedder<LayeredLsh<_>, D> =
+        embedder::WEmbedder::random(42, graph, options);
+
+    embedder.embed_with_callback(|i| {
+        if i % 10 == 0 && i > 0 {
+            // eprintln!("Iteration {i}");
+        }
+    });
+    let pos = embedder.positions();
+
+    let analysis = simulation::radius_reduction::DimReduction::new(pos.to_vec());
+
+    let mut stats_normal = Statistics::default();
+    let mut stats_reduced = Statistics::default();
+    for i in 0..pos.len() {
+        let mut results = Vec::new();
+        analysis.query(i, 1., &mut results, &mut stats_normal, false);
+        analysis.query(i, 1., &mut results, &mut stats_reduced, true);
+    }
+    eprintln!(
+        "normal: {} distance calculations\nreduction: {} distance calculations\n percent: {:.2}%",
+        stats_normal.num_comparionsons,
+        stats_reduced.num_comparionsons,
+        stats_reduced.num_comparionsons as f64 * 100. / stats_normal.num_comparionsons as f64
+    );
+    dbg!(&stats_reduced);
+    println!(
+        "{}, {}, {}",
+        D, stats_normal.num_comparionsons, stats_reduced.num_comparionsons,
+    );
+}
---
+++ simulation/src/radius_reduction.rs
@@ -0,0 +1,249 @@
+use rembed::dvec::DVec;
+use std::ops::Mul;
+
+#[derive(Clone, Debug)]
+pub enum SplitStrategy {
+    Element {
+        num_buckets: usize,
+    },
+    Spatial {
+        resolution: f32,
+        bucket_offset: isize,
+    },
+}
+
+pub struct DimReduction<const D: usize> {
+    positions: Vec<DVec<D>>,
+    root: Layer<D>,
+}
+
+#[derive(Debug)]
+pub enum Layer<const D: usize> {
+    BruteForce(Vec<DVec<D>>),
+    Split {
+        strategy: SplitStrategy,
+        dim: usize,
+        bucket_starts: Vec<(f32, f32)>,
+        children: Vec<Layer<D>>,
+    },
+}
+
+impl<const D: usize> Layer<D> {
+    fn new(mut positions: Vec<DVec<D>>, depth: usize) -> Self {
+        if positions.len() <= 1 {
+            return Self::BruteForce(positions);
+        }
+        let dim = depth % D;
+        // dbg!(positions.len(), dim);
+
+        // Sort by the specified dimension
+        positions.sort_unstable_by(|a, b| a[dim].partial_cmp(&b[dim]).unwrap());
+
+        const NUM_BUCKETS: usize = 2;
+        const RESOLUTION: f32 = 2.;
+
+        let element_split = Self::element_split(positions.clone(), dim, NUM_BUCKETS, depth);
+
+        let spatial_split = Self::spatial_split(positions, dim, RESOLUTION, depth);
+
+        // element_split
+        spatial_split
+    }
+
+    fn element_split(
+        positions: Vec<DVec<D>>,
+        dim: usize,
+        num_buckets: usize,
+        depth: usize,
+    ) -> Self {
+        let mut children = Vec::new();
+        let total_len = positions.len();
+        let mut bucket_starts = Vec::new();
+
+        for i in 0..num_buckets {
+            let start = i * total_len / num_buckets;
+            let end = if i == num_buckets - 1 {
+                total_len
+            } else {
+                (i + 1) * total_len / num_buckets
+            };
+
+            if start < end {
+                let bucket_positions = positions[start..end].to_vec();
+
+                let child = Layer::new(bucket_positions, depth + 1);
+                bucket_starts.push((positions[start][dim], positions[end - 1][dim]));
+                children.push(child);
+            }
+        }
+
+        Self::Split {
+            strategy: SplitStrategy::Element { num_buckets },
+            dim,
+            bucket_starts,
+            children,
+        }
+    }
+
+    fn spatial_split(positions: Vec<DVec<D>>, dim: usize, resolution: f32, depth: usize) -> Self {
+        let min = positions[0][dim].mul(resolution).floor();
+        let max = positions.last().unwrap()[dim].mul(resolution).floor();
+        let num_buckets = (max - min) as usize + 1;
+
+        if num_buckets <= 1 {
+            return Self::BruteForce(positions);
+        }
+
+        let mut buckets = vec![Vec::new(); num_buckets];
+        let bucket_offset = -min as isize;
+
+        for pos in positions {
+            let bucket_key = (pos[dim] * resolution).floor() as isize + bucket_offset;
+            buckets[bucket_key as usize].push(pos);
+        }
+        let bucket_starts: Vec<_> = buckets
+            .iter()
+            .map(|children| {
+                (
+                    children.first().unwrap_or(&DVec::from_fn(|_| -100.))[dim],
+                    children.last().unwrap_or(&DVec::from_fn(|_| 100.))[dim],
+                )
+            })
+            .collect();
+
+        // Convert buckets to children layers
+        let mut children = Vec::new();
+
+        for bucket in buckets.into_iter() {
+            let child = Layer::new(bucket, depth + 1);
+            children.push(child);
+        }
+
+        Self::Split {
+            strategy: SplitStrategy::Spatial {
+                resolution,
+                bucket_offset,
+            },
+            bucket_starts,
+            dim,
+            children,
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct Statistics {
+    pub num_comparionsons: usize,
+    pub num_splits: usize,
+    pub num_reductions: usize,
+}
+
+impl<const D: usize> DimReduction<D> {
+    pub fn new(positions: Vec<DVec<D>>) -> Self {
+        let root = Layer::new(positions.clone(), 0);
+
+        Self { positions, root }
+    }
+    pub fn query(
+        &self,
+        id: usize,
+        radius: f32,
+        results: &mut Vec<usize>,
+        stats: &mut Statistics,
+        use_radius_reduction: bool,
+    ) {
+        self.query_impl(
+            self.positions[id],
+            radius,
+            DVec::zero(),
+            results,
+            0,
+            &self.root,
+            stats,
+            use_radius_reduction,
+        );
+    }
+
+    fn query_impl(
+        &self,
+        pos: DVec<D>,
+        radius: f32,
+        spatial_offset: DVec<D>,
+        results: &mut Vec<usize>,
+        depth: usize,
+        layer: &Layer<D>,
+        statistics: &mut Statistics,
+        use_radius_reduction: bool,
+    ) {
+        // println!("final_radius: {}", 1. - spatial_offset.magnitude());
+        if let Layer::BruteForce(positions) = layer {
+            statistics.num_comparionsons += positions.len();
+            // return;
+        }
+
+        if let Layer::Split {
+            dim,
+            bucket_starts,
+            children,
+            ..
+        } = layer
+        {
+            let p = pos[*dim];
+            // dbg!(p);
+            for (i, ((start, end), child)) in bucket_starts.iter().zip(children.iter()).enumerate()
+            {
+                // dbg!(i, start, end);
+                let mut new_spatial_offset = spatial_offset;
+                if *end <= p {
+                    new_spatial_offset[*dim] = p - *end;
+                }
+                if *start >= p {
+                    new_spatial_offset[*dim] = *start - p;
+                }
+
+                statistics.num_splits += 1;
+                if new_spatial_offset != spatial_offset {
+                    statistics.num_reductions += 1;
+                }
+                // let before_first_or_after_last =
+                //     (p < *start && i == 0) || (p > *end && i == bucket_starts.len() - 1);
+                // // We know that we can't be in this set
+                // if before_first_or_after_last {
+                //     return;
+                // }
+                // assert!(!before_first_or_after_last);
+
+                let should_recurse = (p + radius >= *start && p - radius <= *start)
+                    || (p + radius >= *end && p - radius <= *end)
+                    || (p >= *start && p <= *end);
+                let should_recurse_red = new_spatial_offset.magnitude_squared() < radius.powi(2);
+                if should_recurse_red && !should_recurse {
+                    // println!("skipped check at depth {depth}");
+                    dbg!(
+                        pos,
+                        dim,
+                        new_spatial_offset,
+                        pos - new_spatial_offset,
+                        start,
+                        end
+                    );
+                }
+                if should_recurse_red && use_radius_reduction
+                    || should_recurse && !use_radius_reduction
+                {
+                    // if should_recurse {
+                    self.query_impl(
+                        pos,
+                        radius,
+                        new_spatial_offset,
+                        results,
+                        depth + 1,
+                        child,
+                        statistics,
+                        use_radius_reduction,
+                    );
+                }
+            }
+        }
+    }
+}
