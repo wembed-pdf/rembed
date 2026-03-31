35e2dec8ca5be0e73ba29b77e4ca1a374397d60c
Dennis Kobert <dennis@kobert.dev>
2026-03-31 21:35:18 +0200

    Write embedding to file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use std::time::Instant;
 
-use rembed::{embedder::EmbedderOptions, *};
+use rembed::{embedder::EmbedderOptions, query::Embedder as _, *};
 
 fn main() -> io::Result<()> {
     let graph_name = "rel8";
     // let graph_name = "bio-grid-fruitfly";
 
-    const D: usize = 12;
-    let dim_hint = 8;
+    const D: usize = 2;
+    let dim_hint = D;
 
-    let _graph = format!("data/{}/graph", graph_name);
+    let _graph = &format!("data/{}/graph", graph_name);
     // let graph = "data/generated/graphs/1084_girg_n-1000_deg-25_dim-2_ple-2.5_alpha-inf_wseed-14_pseed-132_sseed-1402";
     // let graph = "data/generated/graphs/19_girg_n-1000_deg-15_dim-4_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
     // let graph = "data/generated/graphs/55_girg_n-10000_deg-15_dim-2_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
-    // let graph = "data/generated/graphs/73_girg_n-10000_deg-15_dim-4_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
+    let graph = "data/generated/graphs/73_girg_n-10000_deg-15_dim-4_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
     // let graph = "data/generated/graphs/109_girg_n-100000_deg-15_dim-2_ple-2.2_alpha-inf_wseed-12_pseed-130_sseed-1400";
     // let graph = "../praktikum-beating-the-worst-case-framework-rust-uwu/instances/graphs/609.gr";
     // let graph = "../praktikum-beating-the-worst-case-framework-rust-uwu/instances/graphs/72.gr";
     // let graph = "../../cpp/sample/edge_lists_real/web-spam";
-    let graph = "../../cpp/sample/edge_lists_real/rel7";
+    // let graph = "../../cpp/sample/edge_lists_real/rel7";
     // let graph = "../../cpp/sample/edge_lists_real/rel6";
+    // let graph = "soc-LiveJournal1.edges";
     let graph = graph::Graph::parse_from_edge_list_file(graph, D, dim_hint)?;
 
     let count = graph.nodes.len();
@@ -39,12 +40,12 @@ fn main() -> io::Result<()> {
         graph: &graph,
     };
 
-    let lossy_queries = ATree::new(&embedding);
+    // let lossy_queries = ATree::new(&embedding);
     // let lossy_queries = Kiddo::new(embedding.clone());
     // let lossy_queries = embedding.clone();
     // let lossy_queries =
     //     RandomProjectionLsh::<_>::new_with_params(embedding.clone(), Some(1), Some(16));
-    // let lossy_queries = DynamicQuery::<_, ATree<_>>::new(&embedding);
+    let lossy_queries = DynamicQuery::<_, ATree<_>>::new(&embedding);
     let mut embedder = embedder::WEmbedder::new(lossy_queries, options);
 
     let start = Instant::now();
@@ -52,7 +53,7 @@ fn main() -> io::Result<()> {
     let mut last_time = Instant::now();
     embedder.embed_with_callback(|e| {
         let i = e.iteration();
-        if i % 100 == 0 && i > 0 {
+        if i % 10 == 0 && i > 0 {
             eprintln!(
                 "Iteration {i}, Δp{}, {:.1}s",
                 e.last_pos_delta().unwrap_or_default(),
@@ -71,6 +72,16 @@ fn main() -> io::Result<()> {
         Default::default(),
     );
     embedder.print_stats();
+
+    use std::fmt::Write as _;
+    let mut out_pos = String::new();
+    for pos in embedder.positions() {
+        for x in pos.components {
+            let _ = write!(&mut out_pos, "{}", x);
+        }
+        let _ = writeln!(&mut out_pos);
+    }
+    let _ = std::fs::write("pos", out_pos);
 
     Ok(())
 }
